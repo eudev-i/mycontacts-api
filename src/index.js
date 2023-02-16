7248d8e69c98eb1db7721bfcfe2f6e7b6c48806c
Vitoria Gonçalves Gaspar <vitoriagoncalvesgaspar@Vitorias-MacBook-Pro.local>
2023-02-16 16:42:29 -0300

    servidor http com express

---
+++ src/index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (request, response) => {
+  response.send('Olá')
+})
+
+app.listen(3000, () => console.log('😄 Servidor rodando na porta http://localhost:3000'));
