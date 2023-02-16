d1c04be36b98a86c6d63aece4231cec5ea480819
Vitoria <vitoria.goncalvess.gaspar@gmail.com>
2023-02-16 20:52:41 -0300

    adicionando nodemon

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (request, response) => {
-  response.send('Olá')
-})
+  response.send('Olá');
+});
 
 app.listen(3000, () => console.log('😄 Servidor rodando na porta http://localhost:3000'));
