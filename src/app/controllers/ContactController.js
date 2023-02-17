d3b354ec65aaa6c80b3982cbe2ef9e309525cd39
Vitoria <vitoria.goncalvess.gaspar@gmail.com>
2023-02-16 21:22:26 -0300

    criando controller de contatos

---
+++ src/app/controllers/ContactController.js
@@ -0,0 +1,28 @@
+class ContactController {
+  // Listar todos os itens
+  index(request, response) {
+    response.send('Index Contact Controller');
+  }
+
+  // Obter UM item
+  show() {
+
+  }
+
+  // Criar um novo item
+  store() {
+
+  }
+
+  // Editar um item
+  update() {
+
+  }
+
+  // Excluir um item
+  delete() {
+
+  }
+}
+
+module.exports = new ContactController();
--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 const express = require('express');
 
+const routes = require('./routes');
+
 const app = express();
 
-app.get('/', (request, response) => {
-  response.send('Olá');
-});
+// Para usar as rotas
+app.use(routes);
 
 app.listen(3000, () => console.log('😄 Servidor rodando na porta http://localhost:3000'));
