1e972691104776a63df794a7bea2a3ce4572c644
Vitoria <vitoria.goncalvess.gaspar@gmail.com>
2023-02-21 01:25:51 -0300

    tratando erros

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 
+require('express-async-errors');
+
 const routes = require('./routes');
 
 const app = express();
@@ -8,5 +10,11 @@ app.use(express.json());
 
 // Middleware para usar as rotas
 app.use(routes);
+
+// Error Handler (Middleware express) -> Manipulador de erros
+app.use((error, request, response, next) => {
+  console.log(error);
+  response.sendStatus(500);
+});
 
 app.listen(3000, () => console.log('😄 Servidor rodando na porta http://localhost:3000'));
